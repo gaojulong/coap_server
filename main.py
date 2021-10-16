4e20cec7cb19f8f58c4b5d56754bcc2d12b953d7
gaojulong <gaojulong@outlook.com>
2021-10-16 09:54:31 +0800

    add weather api

--- main.py
+++ main.py
@@ -31,17 +31,48 @@ class BasicResource(Resource):
         return True
 
 
+class WeatherResource(Resource):
+    def __init__(self, name="BasicResource", coap_server=None):
+        super(WeatherResource, self).__init__(name, coap_server, visible=True,
+                                            observable=True, allow_children=True)
+
+    def render_GET_advanced(self, request, response):
+        pass
+
+    def render_PUT(self, request):
+        pass
+
+    def render_PUT_advanced(self, request, response):
+        pass
+
+    def render_POST(self, request):
+        pass
+
+    def render_POST_advanced(self, request, response):
+        pass
+
+    def render_DELETE(self, request):
+        pass
+
+    def render_DELETE_advanced(self, request, response):
+        pass
+
+    def render_GET(self, request):
+        pass
+
+
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
         self.add_resource('basic', BasicResource())
+        self.add_resource('weather', WeatherResource)
 
 
 def main():
     print("CoAPServer IP addr : %s port : %d " % (Host, Port))
     server = CoAPServer(Host, Port)
     try:
-        server.listen(10)
+        server.listen()
     except KeyboardInterrupt:
         print("Server Shutdown")
         server.close()
