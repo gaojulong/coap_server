19af4a58acda25ee7a159b1058d48137ade6f8bb
gaojulong <gaojulong@outlook.com>
2021-10-16 10:04:48 +0800

    add weather

--- main.py
+++ main.py
@@ -1,71 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
-
 from coapthon.server.coap import CoAP
-from coapthon.resources.resource import Resource
+
+import coap_weather
+
 
 Host = "0.0.0.0"  		# 本机IP地址
 Port = 5683             # 端口号
-
-class BasicResource(Resource):
-    def __init__(self, name="BasicResource", coap_server=None):
-        super(BasicResource, self).__init__(name, coap_server, visible=True,
-                                            observable=True, allow_children=True)
-        self.payload = "Basic Resource"
-
-    def render_GET(self, request):
-        return self
-
-    def render_PUT(self, request):
-        self.payload = request.payload
-        return self
-
-    def render_POST(self, request):
-        res = BasicResource()
-        res.location_query = request.uri_query
-        res.payload = request.payload
-        return res
-
-    def render_DELETE(self, request):
-        return True
-
-
-class WeatherResource(Resource):
-    def __init__(self, name="BasicResource", coap_server=None):
-        super(WeatherResource, self).__init__(name, coap_server, visible=True,
-                                            observable=True, allow_children=True)
-
-    def render_GET_advanced(self, request, response):
-        pass
-
-    def render_PUT(self, request):
-        pass
-
-    def render_PUT_advanced(self, request, response):
-        pass
-
-    def render_POST(self, request):
-        pass
-
-    def render_POST_advanced(self, request, response):
-        pass
-
-    def render_DELETE(self, request):
-        pass
-
-    def render_DELETE_advanced(self, request, response):
-        pass
-
-    def render_GET(self, request):
-        pass
 
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
-        self.add_resource('basic', BasicResource())
-        self.add_resource('weather', WeatherResource)
+        self.add_resource('weather', coap_weather.WeatherResource())
 
 
 def main():
