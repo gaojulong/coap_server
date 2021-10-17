a3f1237d3c7839d0c598b94c08b4ac0e45d891e3
gaojulong <gaojulong@outlook.com>
2021-10-17 14:18:28 +0800

    add weather

--- weather.py
+++ weather.py
@@ -12,61 +12,59 @@ def DEBUG(str):
 
 
 def get_weather_3d(location, key, lang='zh', unit='m'):
+    data = {}
+    list_daily = []
+    data_daily = {}
     url = WEATHER_URL_3D+"location="+location + "&key="+key + "&lang="+lang + "&unit="+unit
     DEBUG(url)
     response = requests.get(url)
     DEBUG(response.text)
     data_json = json.loads(response.text)
-    return data_json
+    code = data_json['code']
+    data['code'] = code
+    if code != "200":
+        return str(data)
+    daily = data_json['daily']
+    for d in daily:
+        data_daily['fxDate'] = d['fxDate']
+        data_daily['tempMin'] = d['tempMin']
+        data_daily['tempMax'] = d['tempMax']
+        data_daily['iconDay'] = d['iconDay']
+        data_daily['textDay'] = d['textDay']
+        list_daily.append(data_daily)
+    data['daily'] = list_daily
+    return str(data)
 
 
 def get_weather_now(location, key, lang='zh', unit='m', gzip='n'):
+    data = {}
     url = WEATHER_URL_NOW + "location=" + location + "&key=" + key + "&lang=" + lang + "&unit=" + unit + "&gzip=" + gzip
     DEBUG(url)
     response = requests.get(url)
     DEBUG(response.text)
     data_json = json.loads(response.text)
-    return data_json
+    code = data_json['code']
+    data['code'] = code
+    if code != "200":
+        return str(data)
 
-
-# 只返回所需要的天气数据
-def get_weather(location, key, lang='zh', unit='m', gzip='n'):
-    success_data = ""
-    error_data = "error"
-    weather_3d = get_weather_3d(location, key, lang, unit)
-    weather_now = get_weather_now(location, key, lang, unit, gzip)
-
-    code_3d = weather_3d['code']
-    code_now = weather_now['code']
-
-    if (code_3d != "200") or (code_now != "200"):
-        return error_data
-
-    now = weather_now['now']
-    temp = now['temp']              # 温度
+    now = data_json['now']
+    temp = now['temp']  # 温度
     icon = now['icon']
-    text = now['text']              # 天气状况 -> 晴
-    # windDir = now['windDir']        # 风向
-    # windScale = now['windScale']    # 风力等级
-    humidity = now['humidity']      # 湿度
-    precip = now['precip']          # 降雨量
-    pressure = now['pressure']      # 大气压
+    text = now['text']  # 天气状况 -> 晴
+    humidity = now['humidity']  # 湿度
 
-    DEBUG(now)
-    DEBUG("温度:"+temp)
-    DEBUG("icon"+icon)
-    DEBUG("天气状况"+text)
-    DEBUG("湿度"+humidity)
-    DEBUG("降雨量"+precip)
-    DEBUG("大气压"+pressure)
+    data['temp'] = temp
+    data['icon'] = icon
+    data['text'] = text
+    data['humidity'] = humidity
+    return str(data)
 
-    dailys = weather_3d['daily']
-    for daily in dailys:
-        DEBUG(daily)
-
-    return success_data
 
 
 if __name__ == '__main__':
-    weather = get_weather("101010100", "9e54b1e3d00e4f36b813065e30b0eec7")
-    print(weather)
+    # weather = get_weather_now("117.282488,31.775297", "9e54b1e3d00e4f36b813065e30b0eec7")
+    # print(weather)
+    weather = get_weather_3d("117.282488,31.775297", "9e54b1e3d00e4f36b813065e30b0eec7")
+    print(json.dumps(weather))
+
